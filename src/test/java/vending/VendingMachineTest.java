239250957ad845042c749571d9ec4b53686350eb
keiakihito <kkatsumi.git@gmail.com>
2025-11-22 15:11:30 -0800

    [Add] Redbar test for initializeSackMapAndChain() for VendingMachine.java

--- src/test/java/vending/VendingMachineTest.java
+++ src/test/java/vending/VendingMachineTest.java
@@ -2,8 +2,11 @@ package vending;
 
 import org.junit.jupiter.api.Test;
 import vending.state.IdleState;
+import vending.state.State;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class VendingMachineTest {
     @Test
@@ -11,5 +14,13 @@ class VendingMachineTest {
         VendingMachine machine = new VendingMachine();
 
         assertTrue(machine.getState() instanceof IdleState, "machine should start in IdleState");
+    }
+
+    @Test
+    void initializesSnackMapAndChain() {
+        VendingMachine machine = new VendingMachine();
+
+        assertNotNull(machine.getSnacks(), "snacks map should be initialized");
+        assertNotNull(machine.getHandler(), "handler chain should be initialized");
     }
 }
